2af6751e2a33424ec032116d1717901c505dffbe
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2023-12-07 11:06:46 +0300

    Update game-prime.js

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -3,10 +3,10 @@ import getRandomNumber from '../randomNumber.js';
 import createGameLogic from '../index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const minNumber = 2;
-const maxNumber = 500;
+const minNumber = 1;
+const maxNumber = 150;
 const isPrime = (num) => {
-  for (let i = 2, max = Math.sqrt(num); i <= max; i += 1) {
+  for (let i = 1, max = Math.sqrt(num); i <= max; i += 1) {
     if (num % i === 0) {
       return false;
     }

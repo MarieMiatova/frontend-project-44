7aa1e3f286b94fa1a4741acdc76c0c58d3d7811b
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2024-04-22 12:16:28 +0300

    Initial commit

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,26 +1,20 @@
 import getRandomNumber from '../randomNumber.js';
 import createGameLogic from '../index.js';
 
-const evenLogic = (description) => {
+const startEvenGame = () => {
+  const description = 'Answer "yes" if the number is even, otherwise answer "no".';
   const minNumber = 1;
   const maxNumber = 25;
 
   const isEven = (number) => number % 2 === 0;
 
-  const question = getRandomNumber(minNumber, maxNumber);
-  const answer = isEven(question) ? 'yes' : 'no';
-
-  return {
-    description,
-    question,
-    answer,
+  const evenLogic = () => {
+    const question = getRandomNumber(minNumber, maxNumber);
+    const answer = isEven(question) ? 'yes' : 'no';
+    return [question, answer];
   };
-};
 
-const startEvenGame = () => {
-  const description = 'Answer "yes" if the number is even, otherwise answer "no".';
-  const { description: gameDescription, question, answer } = evenLogic(description);
-  createGameLogic(gameDescription, question, answer);
+  createGameLogic(description, evenLogic);
 };
 
 export default startEvenGame;
